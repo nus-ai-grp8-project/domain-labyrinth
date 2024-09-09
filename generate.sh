d5d317f28b646fd196d1da843b5bdcc9d662ac51
Tan Kai Min Russell <russelltanreg38@gmail.com>
2024-09-09 17:53:50 +0800

    Add larger instances

--- generate.sh
+++ generate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Loop from 0 to 249
-N=5
+N=101
 
-for i in {0..249}
+for i in {0..4}
 do
   # Create a string with the format instance_{i}.svg
   filename="${N}x${N}_instances_images/instance_${i}_${N}_by_${N}.svg"
   inst_filename="${N}x${N}_instances_pddl/instance_${i}_${N}_by_${N}.pddl"
 
-  python3 generator.py --size=${N} --seed=${i} --num-rotations=2 --image-init=${filename} --pddl=${inst_filename}
+  python3 generator.py --size=${N} --seed=${i} --num-rotations=12 --image-init=${filename} --pddl=${inst_filename}
   # Uncomment the next line if you want to create empty SVG files
   # touch $filename
 done
